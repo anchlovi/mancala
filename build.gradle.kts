ffc9d91d763b5e41018942d445d471d97c69648f
Shachar Anchelovich <sanchelovich@microsoft.com>
2023-11-12 10:59:00 +0200

    Initial repo structure

---
+++ build.gradle.kts
@@ -0,0 +1,87 @@
+import kotlin.io.path.absolutePathString
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    val kotlinVersion = "1.9.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("nu.studer.jooq") version "8.2"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.jooq:jooq")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mysql")
+
+    jooqGenerator("com.mysql:mysql-connector-j")
+    jooqGenerator("org.jooq:jooq-meta-extensions")
+}
+
+jooq {
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.WARN
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.extensions.ddl.DDLDatabase"
+                        properties = listOf(
+                                org.jooq.meta.jaxb.Property().apply {
+                                    key = "scripts"
+                                    value = sourceSets.main.map {
+                                        it.resources.sourceDirectories.singleFile
+                                                .toPath()
+                                                .resolve("db/scripts/01.sql")
+                                                .absolutePathString()
+                                    }.get()
+                                },
+                                org.jooq.meta.jaxb.Property().apply {
+                                    key = "defaultNameCase"
+                                    value = "lower"
+                                }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") { allInputsDeclared.set(true) }
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    jvmArgs("-Xshare:off")
+}
+
+tasks.bootBuildImage {
+    builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
---
+++ src/test/kotlin/com/example/mancalaapp/ApplicationContextTest.kt
@@ -0,0 +1,15 @@
+package com.example.mancalaapp
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+internal class ApplicationContextTest {
+
+    @Test
+    internal fun `Application context is correctly set up`() {
+    }
+}
