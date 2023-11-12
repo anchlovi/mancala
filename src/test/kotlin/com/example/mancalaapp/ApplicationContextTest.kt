1c4e54cf2fda881b79a5c8eda19bf5317db1c658
Shachar Anchelovich <sanchelovich@microsoft.com>
2023-11-12 11:38:07 +0200

    Imports

--- src/test/kotlin/com/example/mancalaapp/ApplicationContextTest.kt
+++ src/test/kotlin/com/example/mancalaapp/ApplicationContextTest.kt
@@ -1,6 +1,5 @@
 package com.example.mancalaapp
 
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
