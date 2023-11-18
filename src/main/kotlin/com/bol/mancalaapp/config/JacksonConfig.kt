97b7c0532f093e2d352f8816529bd28ccdc95949
Shachar Anchelovich <sanchelovich@microsoft.com>
2023-11-18 10:47:24 +0200

    align json naming strategy

--- src/main/kotlin/com/bol/mancalaapp/config/JacksonConfig.kt
+++ src/main/kotlin/com/bol/mancalaapp/config/JacksonConfig.kt
@@ -3,15 +3,18 @@ package com.bol.mancalaapp.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.swagger.v3.core.jackson.ModelResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class JacksonConfig {
     @Bean
-    fun objectMapper(): ObjectMapper {
-        return ObjectMapper()
+    fun objectMapper(): ObjectMapper =
+        ObjectMapper()
             .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
             .registerModule(KotlinModule.Builder().build())
-    }
+
+    @Bean
+    fun modelResolver(objectMapper: ObjectMapper): ModelResolver = ModelResolver(objectMapper)
 }
